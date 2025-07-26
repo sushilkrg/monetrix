b0cf325419becbd0b9a25141f28a9f618cb90980
Sushil Kumar <sushilkr820@gmail.com>
2025-07-26 14:42:04 +0530

    added authentication, middleware and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Monetrix",
+    default: "Monetrix",
+  },
+  description:
+    "Monetrix is a finance dashboard app in which registered users can add, edit, delete, and manage their invoices.",
+  metadataBase: new URL("https://monetrix.vercel.app"),
+};
 
 export default function RootLayout({
   children,
