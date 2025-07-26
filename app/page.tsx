49f1b0716a2e480944c6e1f453e6d2350b79d2b6
Sushil Kumar <sushilkr820@gmail.com>
2025-07-26 20:16:39 +0530

    changes main page ui images and auth config

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,7 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
-            <a href="https://nextjs.org/learn/" className="text-gray-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Monetrix.</strong> This is a finance dashboard app in which registered users can manage their invoices.
           </p>
           <Link
             href="/login"
@@ -31,14 +27,14 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-desktop.png"
+            src="/monetrix-dashboard.png"
             width={1000}
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
-            src="/hero-mobile.png"
+            src="/monetrix-mobile-dashboard.png"
             width={560}
             height={620}
             className="block md:hidden"
